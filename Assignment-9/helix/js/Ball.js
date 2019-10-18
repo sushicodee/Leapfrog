9b4951b28ea1cd8048a6f0aab1417f0edc85da73
sushicodee <ssushant.sss@gmail.com>
2019-10-18 07:41:41 +0545

    added condition for style scale transform

--- Assignment-9/helix/js/Ball.js
+++ Assignment-9/helix/js/Ball.js
@@ -1,90 +1,91 @@
 class Ball {
-    constructor(parentElem, x, y, radius, color, id) {
-      this.parentElem = parentElem;
-      this.id = id;
-      this.x = x;
-      this.y = y;
-      this.radius = radius;
-      this.diameter = 2 * radius;
-      this.color = color;
-      this.element;
-      this.counter = 0.1;
-      this.frequency = 1;
-      console.log(color);
+  constructor(parentElem, x, y, radius, color, id) {
+    this.parentElem = parentElem;
+    this.id = id;
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.diameter = 2 * radius;
+    this.color = color;
+    this.element;
+    this.counter = 0.1;
+    this.frequency = 1;
+  }
+
+  domDrawBall = function() {
+    this.element = document.createElement("span");
+    this.parentElem.appendChild(this.element);
+    this.element.setAttribute("class", "Ball");
+    this.element.setAttribute("id", this.id);
+    this.element.style.position = "absolute";
+    this.element.style.left = `${this.x}px`;
+    this.element.style.top = `${this.y}px`;
+    this.element.style.width = `${this.diameter}px`;
+    this.element.style.height = `${this.diameter}px`;
+    this.element.style.backgroundImage = `url('./assets/images/moving.gif')`;
+    this.element.style.backgroundSize = "contain";
+    this.element.style.backgroundRepeat = "round";
+    this.element.style.borderRadius = "50%";
+    this.element.style.backgroundColor = `${this.color}`;
+  };
+  
+  setPosition = function(y) {
+    this.y = y;
+  };
+
+  drawPositionChange() {
+    this.element.style.top = this.y + "px";
+  }
+
+  scaleBalls = function(position, i,j) {
+    if(position == 'bottom'){
+    let change = Math.cos(i + 1 * this.counter - j / 4);
+    let change1 = Math.sin(i + 1 * this.counter- j / 4);
+    change = (change > 0) ? change : change1/4;    
+    this.element.style.zIndex = (change > 0) ? 10:0; 
+    this.element.style.transform = `scale(${change})`;
     }
+    let change = Math.sin(i + 1 * this.counter - j / 4);
+    let change1 = Math.cos(i + 1 * this.counter- j / 4);
+    if(position == 'top'){
+    change = (change > 0) ? change : change1/4;    
+    this.element.style.zIndex = (change > 0) ? 10:0; 
+    this.element.style.transform = `scale(${change})`;
 
-    domDrawBall = function() {
-      this.element = document.createElement("span");
-      this.parentElem.appendChild(this.element);
-      this.element.setAttribute("class", "Ball");
-      this.element.setAttribute("id", this.id);
-      this.element.style.position = "absolute";
-      this.element.style.left = `${this.x}px`;
-      this.element.style.top = `${this.y}px`;
-      this.element.style.width = `${this.diameter}px`;
-      this.element.style.height = `${this.diameter}px`;
-      this.element.style.backgroundImage = `url('./assets/images/moving.gif')`;
-      this.element.style.backgroundSize = "contain";
-      this.element.style.backgroundRepeat = "round";
-      this.element.style.borderRadius = "50%";
-      this.element.style.backgroundColor = `${this.color}`;
-     
-    };
-    draw = function() {
-      this.element.style.top = this.y + "px";
-      this.element.style.left = this.x + "px";
-    };
-
-    setPosition = function(y) {
-        this.y = y;
-    };
-
-    drawPositionChange(){
-        this.element.style.top = this.y + "px";
     }
+   
+  };
 
-    scaleBalls = function(position,i){
-        if(position == 'top'){
-                this.element.style.transform = `scale(${Math.cos(i+1 * this.counter)})`;
-            }else{
-                if(position == 'bottom'){
-                    this.element.style.transform = `scale(${Math.sin(i+1 * this.counter)})`;
-                }
-
-            }
+  animateBall(position, height, amplitude, i,j) {
+  
+    this.counter = this.counter + 0.01;
+    if(this.counter == 1){
+        this.counter = 0;
     }
+    if (position == "top") {
+    //   let y =  height / 2 + Math.sin(this.y * this.counter) * amplitude  ;
+    let y =  
+        height / 2 -this.diameter * j  +
+        Math.sin(i + 1 * this.frequency * this.counter) * amplitude;
 
-    animateBall(position,height,amplitude,i){
         
-        // switch(this.counter){
-        //     case 0 : this.counter = this.counter + 0.01;
-        //         break;
-        //     case 1 : this.counter = this.counter - 0.01;
-        //         break;
-        //     default :break;    
-        // }
-        this.counter = this.counter + 0.01;
-        // this.counter = this.counter % 1 === 0? this.counter - 0.01: this.counter +0.01;
-        if(position == 'top'){           
-            let y = height/2 + Math.sin(i + 1 * this.frequency * this.counter) * amplitude;
-            this.setPosition(y);
+      this.setPosition(y);
+      this.drawPositionChange();
+      this.scaleBalls(position, i,j);
+      // this.element.style.transform = `scale(${Math.cos(i+1 *this.counter - j/4)})`;
+    } else {
+      if (position == "bottom") {
+        // let y = this.y + Math.cos( i + 1  * this.counter ) 
+        let y =
+          height / 2  -this.diameter * j+
+          Math.cos(i + 1 * this.frequency * this.counter) * amplitude;
+        this.setPosition(y);
         this.drawPositionChange();
-        this.scaleBalls(position,i);
-        // this.element.style.transform = `scale(${Math.cos(i+1 *this.counter)})`;
-        }
-        else{
-            if(position == 'bottom'){              
-                let y = height/2 + Math.cos(i + 1 * this.frequency * this.counter) * amplitude;
-                this.setPosition(y);
-                this.drawPositionChange();
-                this.scaleBalls(position,i);
+        this.scaleBalls(position, i,j);
 
-                // this.element.style.transform = `scale(${Math.sin(i+1 *this.counter)})`;
-            }
-            
-        }
-        
+        // this.element.style.transform = `scale(${Math.sin(i+1 *this.counter - j/4)})`;
+      }
     }
+  }
 }
 export default Ball;
-   
--- Assignment-9/helix/js/Helix.js
+++ Assignment-9/helix/js/Helix.js
@@ -1,65 +1,147 @@
-
-import Ball from './Ball.js';
+import Ball from "./Ball.js";
 
 class Helix {
-    constructor(width,height,backgroundColor,numberOfBalls){
-        this.FPS = 60;
-        this.FRAME_RATE = 1000 / this.FPS;
-        this.width = width;
-        this.height = height;
-        this.backgroundColor = backgroundColor;
-        this.numberOfBalls = numberOfBalls;
-        this.ballsTop = [];
-        this.ballsBottom = [];
+  constructor(width, height, backgroundColor, numberOfBalls) {
+    this.FPS = 60;
+    this.FRAME_RATE = 1000 / this.FPS;
+    this.width = width;
+    this.height = height;
+    this.backgroundColor = backgroundColor;
+    this.numberOfBalls = numberOfBalls;
+    this.ballsTop = [];
+    this.ballsBottom = [];
+    this.ballRadius = 5;
+    this.ballGap = 10;
+    this.element;
+    this.amplitude = 40;
+    this.waveLength = 0.001;
+    this.createDomStart();
+    this.mainLoop();
+  }
 
-        this.ballGap = 10;
-        this.element;
-        this.amplitude = 20;
-        this.waveLength = 0.001;
-        this.createDomStart();
-        this.mainLoop();
+  createDomStart = function() {
+    this.parenElement = document.getElementsByClassName("helix-container")[0];
+    this.element = document.createElement("div");
+    this.parenElement.appendChild(this.element);
+    this.element.setAttribute("class", `helix-inner-container`);
+    this.element.style.overflow = "hidden";
+    this.element.style.width = `${this.width}px`;
+    this.element.style.height = `${this.height}px`;
+    this.element.style.backgroundColor = this.backgroundColor;
+    this.element.style.border = `1px solid black`;
+    this.element.style.margin = "0 auto";
+    this.element.style.marginTop = "30px";
+    this.element.style.position = "relative";
+  };
+
+  mainLoop = function() {
+    //create balls and draw them
+    for (let i = 0; i < this.numberOfBalls; i++) {
+      let ball = new Ball(
+        this.element,
+        i * this.ballGap - 40,
+        this.height / 2 + Math.sin(i) * this.amplitude,
+        this.ballRadius,
+        "red",
+        i
+      );
+      let ball1 = new Ball(
+        this.element,
+        i * this.ballGap - 40,
+        this.height / 2 - this.ballGap + Math.sin(i) * this.amplitude,
+        this.ballRadius,
+        "red",
+        i
+      );
+      let ball2 = new Ball(
+        this.element,
+        i * this.ballGap - 40,
+        this.height / 2 - this.ballGap * 2 + Math.sin(i) * this.amplitude,
+        this.ballRadius,
+        "red",
+        i
+      );
+      let ball3 = new Ball(
+        this.element,
+        i * this.ballGap - 40,
+        this.height / 2 - this.ballGap * 3 + Math.sin(i) * this.amplitude,
+        this.ballRadius,
+        "red",
+        i
+      );
+      ball.domDrawBall();
+      ball1.domDrawBall();
+      ball2.domDrawBall();
+      ball3.domDrawBall();
+
+      this.ballsTop.push(ball);
+      this.ballsTop.push(ball1);
+      this.ballsTop.push(ball2);
+      this.ballsTop.push(ball3);
     }
+    for (let i = 0; i < this.numberOfBalls; i++) {
+      let ball = new Ball(
+        this.element,
+        i * this.ballGap,
+        this.height / 2 + Math.cos(i) * this.amplitude,
+        5,
+        "white",
+        i
+      );
+      let ball1 = new Ball(
+        this.element,
+        i * this.ballGap,
+        this.height / 2 - this.ballGap + Math.cos(i) * this.amplitude,
+        this.ballRadius,
+        "white",
+        i
+      );
+      let ball2 = new Ball(
+        this.element,
+        i * this.ballGap,
+        this.height / 2 - this.ballGap * 2 + Math.cos(i) * this.amplitude,
+        this.ballRadius,
+        "white",
+        i
+      );
+      let ball3 = new Ball(
+        this.element,
+        i * this.ballGap,
+        this.height / 2 - this.ballGap * 3 + Math.cos(i) * this.amplitude,
+        this.ballRadius,
+        "white",
+        i
+      );
+      ball.domDrawBall();
+      ball1.domDrawBall();
+      ball2.domDrawBall();
+      ball3.domDrawBall();
 
-    createDomStart = function() {
-        this.parenElement = document.getElementsByClassName(
-          "helix-container"
-        )[0];
-        this.element = document.createElement("div");
-        this.parenElement.appendChild(this.element);
-        this.element.setAttribute("class", `helix-inner-container`);
-        this.element.style.overflow = 'hidden';
-        this.element.style.width = `${this.width}px`;
-        this.element.style.height = `${this.height}px`;
-        this.element.style.backgroundColor = this.backgroundColor;
-        this.element.style.border = `1px solid black`;
-        this.element.style.margin = "0 auto";
-        this.element.style.marginTop = "30px";
-        this.element.style.position = "relative";
+      this.ballsBottom.push(ball);
+      this.ballsBottom.push(ball1);
+      this.ballsBottom.push(ball2);
+      this.ballsBottom.push(ball3);
     }
-
-    mainLoop = function(){
-
-        //create balls and draw them
-        for(let i = 0; i<this.numberOfBalls;i++){
-            let ball = new Ball(this.element, i * this.ballGap - 50, this.height/2 + Math.sin(i) * this.amplitude , 5, 'white',i);
-            ball.domDrawBall();
-            this.ballsTop.push(ball);
+    const loop = setInterval(() => {
+      for (let i = 0; i < this.numberOfBalls * 4; i++) {
+        for (let j = 1; j <= 4; j++) {
+          this.ballsTop[i].animateBall(
+            "top",
+            this.height,
+            this.amplitude,
+            i,
+            j
+          );
+          this.ballsBottom[i].animateBall(
+            "bottom",
+            this.height,
+            this.amplitude,
+            i,
+            j
+          );
         }
-        for(let i = 0; i<this.numberOfBalls;i++){
-            let ball = new Ball(this.element, i * this.ballGap ,this.height/2 + Math.cos(i) * this.amplitude , 5, 'red',i);
-            ball.domDrawBall();
-            this.ballsBottom.push(ball);
-        }
-        const loop = setInterval(() => {
-            for(let i = 0; i< this.numberOfBalls; i++){
-                this.ballsTop[i].animateBall('top',this.height,this.amplitude,i);
-                this.ballsBottom[i].animateBall('bottom',this.height,this.amplitude,i);
-            }
-
-            
-        },this.FRAME_RATE)
-
-    }
-
+      }
+    }, this.FRAME_RATE);
+  };
 }
 export default Helix;
